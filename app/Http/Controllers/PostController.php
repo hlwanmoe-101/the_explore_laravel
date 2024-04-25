5662be98cb760d0b50ffa886fdcb668782373bba
Hlwan <76532858+hlwanmoe-101@users.noreply.github.com>
2024-04-25 11:36:36 +0700

    Update PostController.php
    mail mute

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -79,7 +79,7 @@ class PostController extends Controller implements ShouldQueue
 
         //mail send
 //        Mail::to('dmoez24057@gmail.com')->send(new PostMailAws($post));
-        Mail::to('dmoez24057@gmail.com')->later(now()->addSecond(5),new PostMailAws($post));
+        //Mail::to('dmoez24057@gmail.com')->later(now()->addSecond(5),new PostMailAws($post));
 
         return redirect()->route('index');
 
